7272eead28e110796644afbccbd6179159ec0a1d
BiieLNTC <106771590+BiieLNTC@users.noreply.github.com>
2023-04-30 19:33:45 -0300

    Add files via upload
    Páginas Grupo de Carros, Área para confirmar reserva e Cadastrar adicionadas, emprementado API para consulta de CEP

---
+++ web/cep.js
@@ -0,0 +1,74 @@
+const addressForm = document.querySelector("#appCep");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#logradouro");
+const cityInput = document.querySelector("#cidade");
+const neighborhoodInput = document.querySelector("#bairro");
+const regionInput = document.querySelector("#estado");
+const complemento = document.querySelector("#complemento")
+const formInputs = document.querySelectorAll("[data-input]");
+
+// valida CEP
+cepInput.addEventListener("keypress", (e) => {
+  const onlyNumbers = /[0-9]|\./;
+  const key = String.fromCharCode(e.keyCode);
+
+  // travar para digitar apenas numeros
+  if (!onlyNumbers.test(key)) {
+    e.preventDefault();
+    return;
+  }
+});
+
+// evento keyup
+cepInput.addEventListener("keyup", (e) => {
+  const inputValue = e.target.value;
+
+  //   ver se ja digitou um cep
+  if (inputValue.length === 8) {
+    getAddress(inputValue);
+  }
+});
+
+// pega o endereço na API
+const getAddress = async (cep) => {
+  
+
+  cepInput.blur();
+
+  const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+
+  const response = await fetch(apiUrl);
+
+  const data = await response.json();
+
+  console.log(data);
+  console.log(formInputs);
+  console.log(data.erro);
+
+  // Show error and reset form
+  if (data.erro === "true" || data.erro === true) {
+    alert('"CEP inválido, tente novamente."')
+    addressForm.reset()
+    return;
+  }
+
+  // Activate disabled attribute if form is empty
+  addressInput.value = data.logradouro;
+  cityInput.value = data.localidade;
+  neighborhoodInput.value = data.bairro;
+  complemento.value = data.complemento;
+  regionInput.value = data.uf;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
